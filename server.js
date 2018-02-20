b814122e7644502ecbc1ea8b6306c7f52a3e04a3
navdeep-2407 <36537655+navdeep-2407@users.noreply.github.com>
2018-02-20 23:59:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article-one', function (req, res) {
-  res.send('this is first article will be served');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/Article-two', function (req, res) {
   res.send('this is Second article will be served');
